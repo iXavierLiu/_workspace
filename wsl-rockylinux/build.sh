d63019e0eef0d4a30f690ed8f4243fe1cebf96af
Xavier Liu <i@xavierliu.io>
2025-04-11 19:50:43 +0800

    update files

--- wsl-rockylinux/build.sh
+++ wsl-rockylinux/build.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
+# Edit the following variables
 IMAGE_NAME=wsl-rockylinux9
+
+# Construction variable
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 CONTAINER_NAME=${IMAGE_NAME}-container
+OUTPUT_FILE=${SCRIPT_DIR}/build/${CONTAINER_NAME}.wsl
+DOCKER_CLI=podman # or docker
 
-docker build -t ${IMAGE_NAME} .
 
-docker create --name ${CONTAINER_NAME} ${IMAGE_NAME}
+$DOCKER_CLI build -t $IMAGE_NAME .
 
-docker export ${CONTAINER_NAME} -o ${SCRIPT_DIR}/build/${CONTAINER_NAME}.tar
+$DOCKER_CLI create --name $CONTAINER_NAME $IMAGE_NAME
 
-docker rm ${CONTAINER_NAME}
+$DOCKER_CLI export $CONTAINER_NAME -o $OUTPUT_FILE
+
+$DOCKER_CLI rm $CONTAINER_NAME
