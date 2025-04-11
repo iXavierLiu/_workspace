1caeaca6e3d681061c281f77fd908d8d83de6291
Xavier Liu <i@xavierliu.io>
2025-04-11 18:00:41 +0800

    update files

--- wsl-rockylinux/build.sh
+++ wsl-rockylinux/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
+IMAGE_NAME=wsl-rockylinux9
+CONTAINER_NAME=${IMAGE_NAME}-container
 
-docker build -t wsl-rockylinux9 .
+docker build -t ${IMAGE_NAME} .
 
-docker create --name wsl-rockylinux9-container wsl-rockylinux9
+docker create --name ${CONTAINER_NAME} ${IMAGE_NAME}
 
-docker export wsl-rockylinux9-container -o ${SCRIPT_DIR}/build/wsl-rockylinux9-container.tar
+docker export ${CONTAINER_NAME} -o ${SCRIPT_DIR}/build/${CONTAINER_NAME}.tar
 
-docker rm wsl-rockylinux9-container
+docker rm ${CONTAINER_NAME}
