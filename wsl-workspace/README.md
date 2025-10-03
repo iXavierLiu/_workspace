20ab113555073edc3c519a0e40662a7cecf45171
Xavier Liu <i@xavierliu.io>
2025-10-04 03:30:39 +0800

    update new settings

---
+++ wsl-workspace/README.md
@@ -0,0 +1,7 @@
+```shell
+sh build.sh
+```
+使用windows的提示符，这里用的是git-bash
+```cmd
+wsl.exe --install --name almalinux-workspace --from-file wsl-almalinux-10.0_20251004_025124.wsl --location /e/WSL/almalinux-workspace
+```
--- wsl-workspace/build.sh
+++ wsl-workspace/build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-IMAGE_NAME=wsl-workspace
-CONTAINER_NAME=${IMAGE_NAME}-container
+set -ue
 
-docker build -t ${IMAGE_NAME} .
+# 根据构建修改
+SRC_IMG="AlmaLinux-10.0_x64_20250529.0.wsl"
+SRC_OS="almalinux"
+SRC_VERSION="10.0"
 
-docker create --name ${CONTAINER_NAME} ${IMAGE_NAME}
+# 变量
+BUILD_IMG="wsl-build"
+DST_NAME="wsl-${SRC_OS}-${SRC_VERSION}"
 
-docker export ${CONTAINER_NAME} -o ${SCRIPT_DIR}/build/${CONTAINER_NAME}.tar
+SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
-docker rm ${CONTAINER_NAME}
+
+podman import $SRC_IMG $BUILD_IMG
+
+podman build -t $DST_NAME .
+
+# 因为要导出rootfs而不是完整镜像，所以要先创建容器
+podman create --name $DST_NAME $DST_NAME
+
+podman export $DST_NAME -o "${SCRIPT_DIR}/build/${DST_NAME}_$(date +%Y%m%d_%H%M%S).wsl"
+
+podman rmi -f $BUILD_IMG $DST_NAME
